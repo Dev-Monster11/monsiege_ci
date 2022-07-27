1d19b2b33c9947b89b9e98a5b39b87001840783d
Dev-Monster11 <softdrink1991@gmail.com>
2022-07-28 00:23:16 +0300

    Test Get

--- application/controllers/api/Staff.php
+++ application/controllers/api/Staff.php
@@ -10,15 +10,10 @@ class Staff extends REST_Controller {
         parent::__construct();
         
     }
-    public function login_post(){
-        $email = $this->post('email');
-        $password = $this->post('password');
+
+    private function searchStaff($val){
         $urls = ['https://kraainem.monsiegesocial.be', 'https://bruxelles.monsiegesocial.be', 'https://overijse.monsiegesocial.be'];
         $tokens = ['eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoibW9iaWxlIiwibmFtZSI6Im1vYmlsZSIsIkFQSV9USU1FIjoxNjU4ODU3NDA3fQ.XpRi1xqMhRltL4b4iReVboqGYME8JZdpESvFZrfaUsQ', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoic3RhZmZfbW9iaWxlIiwibmFtZSI6InN0YWZmX21vYmlsZSIsIkFQSV9USU1FIjoxNjU4ODYxNDIwfQ.AWRB9c1Uqy2fVk0dIkf_qPKQZBu3y8Ql-OuiwnRSDgc', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoibW9iaWxlIiwibmFtZSI6Im1vYmlsZSIsIkFQSV9USU1FIjoxNjU4ODU3NjMzfQ.JLACceWKpUmkHWZ94YkVuuEy4N28dai0l88dByhF0xI'];
-        $staff = array();
-
-        
-        $errorFlag = true;
         for($i = 0; $i < 3; $i++){
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_URL, $urls[$i].'/api/staffs/search/'.$email);
@@ -31,29 +26,55 @@ class Staff extends REST_Controller {
             $response = curl_exec($ch);
             if(!curl_errno($ch))
             {
-
-            //     array_push($result, curl_errorno($ch));
-            // } else {
-
-                // check the HTTP status code of the request
                 $resultStatus = curl_getinfo($ch, CURLINFO_HTTP_CODE);
                 if ($resultStatus == 200) {
-                    // array_push($result, $resultStatus);
-
-                    // die('Request failed: HTTP status code: ' . $resultStatus);
-                    
-                // }else {
                     $errorFlag = false;
                     
                     $staff = (array)json_decode($response);
                     curl_close($ch);
-                    break;
+                    return $staff[0];
                 }
             }
             curl_close($ch);
+        }        
+        return false;
+    }
+    public function login_post(){
+        $email = $this->post('email');
+        $password = $this->post('password');
+        // $urls = ['https://kraainem.monsiegesocial.be', 'https://bruxelles.monsiegesocial.be', 'https://overijse.monsiegesocial.be'];
+        // $tokens = ['eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoibW9iaWxlIiwibmFtZSI6Im1vYmlsZSIsIkFQSV9USU1FIjoxNjU4ODU3NDA3fQ.XpRi1xqMhRltL4b4iReVboqGYME8JZdpESvFZrfaUsQ', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoic3RhZmZfbW9iaWxlIiwibmFtZSI6InN0YWZmX21vYmlsZSIsIkFQSV9USU1FIjoxNjU4ODYxNDIwfQ.AWRB9c1Uqy2fVk0dIkf_qPKQZBu3y8Ql-OuiwnRSDgc', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoibW9iaWxlIiwibmFtZSI6Im1vYmlsZSIsIkFQSV9USU1FIjoxNjU4ODU3NjMzfQ.JLACceWKpUmkHWZ94YkVuuEy4N28dai0l88dByhF0xI'];
+        // $staff = array();
+
+        
+        // $errorFlag = true;
+        // for($i = 0; $i < 3; $i++){
+        //     $ch = curl_init();
+        //     curl_setopt($ch, CURLOPT_URL, $urls[$i].'/api/staffs/search/'.$email);
+        //     $headers = array(
+        //         'Content-Type: application/json',
+        //         'authtoken: '.$tokens[$i]
+        //     );
+        //     curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        //     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        //     $response = curl_exec($ch);
+        //     if(!curl_errno($ch))
+        //     {
+        //         $resultStatus = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        //         if ($resultStatus == 200) {
+        //             $errorFlag = false;
+                    
+        //             $staff = (array)json_decode($response);
+        //             curl_close($ch);
+        //             break;
+        //         }
+        //     }
+        //     curl_close($ch);
             
-        }
-        if ($errorFlag == true){
+        // }
+        $staff = searchStaff($email);
+        
+        if ($staff == false){
             $this->response([
                 'error'     => $errorFlag,
                 'message'   => 'This account does not exist' 
@@ -76,25 +97,13 @@ class Staff extends REST_Controller {
                     'token'         => $token,
                 ]
                 ], 200);
-            // echo '{ 
-            //         "error" : false,
-            //         "message" : "successful",
-            //         "data" : { 
-            //                 "staffId": '.$staff["staffid"].',
-            //                 "email" : "'.$email.'",
-            //                 "firstName" : "'.$staff["firstname"].'",
-            //                 "lastName" : "'.$staff["lastname"].'",
-            //                 "lastLogin" : "'.$staff["lastLogin"].'",
-            //                 "lastActivity" : "'.$staff["lastActivity"].'",
-            //                 "token" : "'.$token.'"
-            //                 }
-            //       }';
         }
     }
     
     public function search_post(){
         $token = $this->post('token');
-
+        $staffId = $this->post('staffId');
+        
     }
 }   
 ?>
