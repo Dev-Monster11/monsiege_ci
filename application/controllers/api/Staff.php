ba0db031eaee8c7f269bde5ff61b3b87a04335a1
Dev-Monster11 <softdrink1991@gmail.com>
2022-07-27 23:29:36 +0300

    Test Get

--- application/controllers/api/Staff.php
+++ application/controllers/api/Staff.php
@@ -19,8 +19,8 @@ class Staff extends REST_Controller {
         $tokens = ['eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoibW9iaWxlIiwibmFtZSI6Im1vYmlsZSIsIkFQSV9USU1FIjoxNjU4ODU3NDA3fQ.XpRi1xqMhRltL4b4iReVboqGYME8JZdpESvFZrfaUsQ', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoic3RhZmZfbW9iaWxlIiwibmFtZSI6InN0YWZmX21vYmlsZSIsIkFQSV9USU1FIjoxNjU4ODYxNDIwfQ.AWRB9c1Uqy2fVk0dIkf_qPKQZBu3y8Ql-OuiwnRSDgc', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoibW9iaWxlIiwibmFtZSI6Im1vYmlsZSIsIkFQSV9USU1FIjoxNjU4ODU3NjMzfQ.JLACceWKpUmkHWZ94YkVuuEy4N28dai0l88dByhF0xI'];
         $result = array();
         for($i = 0; $i < 3; $i++){
-            echo $urls[$i].'/api/staffs/search/'.$email;
-            $this->curl->set_url($urls[$i].'/api/staffs/search/'.$email);
+            echo $urls[$i].'/api/staffs/search/'.htmlspecialchars($email);
+            $this->curl->set_url($urls[$i].'/api/staffs/search/'.htmlspecialchars($email));
             $this->curl->set_method('get');
             $this->curl->setToken($tokens[$i]);
             $response = $this->curl->result();
@@ -35,14 +35,14 @@ class Staff extends REST_Controller {
         $tokens = ['eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoibW9iaWxlIiwibmFtZSI6Im1vYmlsZSIsIkFQSV9USU1FIjoxNjU4ODU3NDA3fQ.XpRi1xqMhRltL4b4iReVboqGYME8JZdpESvFZrfaUsQ', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoic3RhZmZfbW9iaWxlIiwibmFtZSI6InN0YWZmX21vYmlsZSIsIkFQSV9USU1FIjoxNjU4ODYxNDIwfQ.AWRB9c1Uqy2fVk0dIkf_qPKQZBu3y8Ql-OuiwnRSDgc', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoibW9iaWxlIiwibmFtZSI6Im1vYmlsZSIsIkFQSV9USU1FIjoxNjU4ODU3NjMzfQ.JLACceWKpUmkHWZ94YkVuuEy4N28dai0l88dByhF0xI'];
         $result = array();
         for($i = 0; $i < 3; $i++){
-            echo $urls[$i].'/api/staffs/search/'.$email;
-            $this->curl->set_url($urls[$i].'/api/staffs/search/'.$email);
+            echo $urls[$i].'/api/staffs/search/'.htmlspecialchars($email);
+            $this->curl->set_url($urls[$i].'/api/staffs/search/'.htmlspecialchars($email));
             $this->curl->set_method('get');
             $this->curl->setToken($tokens[$i]);
             $response = $this->curl->result();
             array_push($result, $response);
         }
-        $this->response($result, 200);        
+        $this->response($result, 200);   
     }
     public function index_get()
     {
