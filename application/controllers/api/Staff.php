4695df44a44d2385e0c656101859730ee9d7d758
Dev-Monster11 <softdrink1991@gmail.com>
2022-07-29 04:26:43 +0300

    Search Test

--- application/controllers/api/Staff.php
+++ application/controllers/api/Staff.php
@@ -133,6 +133,10 @@ class Staff extends REST_Controller {
         $searchText = $this->post('searchText');
         $isOcr = $this->post('isOcr');
         $staff = $this->searchStaffById($staffId);
+        var_dump(substr(strrev($staff->password), 5, 15));
+        echo "\n";
+        var_dump($token);
+        return;
         if(substr(strrev($staff->password), 5, 15) == $token) {
             $search_text = strtolower($searchText);
             $company_name = $search_text;
